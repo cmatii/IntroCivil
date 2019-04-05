98b3df6c40da99ef4f657dff38157b04e857eae6
cmatii <matias.vargas.marin@gmail.com>
2019-04-05 11:54:37 -0300

    Agregando codigo python
    :)

---
+++ introcivil/codigo-prueba.py
@@ -0,0 +1,33 @@
+# Igresar 3 notas, calcular promedio 
+#y determinar si va a examen < 4.0
+
+
+#N=int(input("Ingresa numero: "))
+#10
+i=1
+y=int(input("Ingresa numero: "))
+
+if(i==y):
+    print("iguales")
+elif(i!=y):
+    print("Son distintos")
+else:
+    print("Error")
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+while(i<=N):
+    if(i%2!=0):
+        print(i)
+    i=i+1
+    '''
